179f4a4f60be5a86ea5f2210919978b2b517e0b9
Singh <singh@Singhs-MacBook-Pro.local>
2022-12-14 21:07:42 -0800

    initial commit

---
+++ challenges/front_end_mock_up_test.js
@@ -0,0 +1,20 @@
+/**
+ Write a function that takes two numbers and returns the sum of those numbers
+
+*/
+
+function add(number1, number2) {
+  return number1 + number2;
+
+}
+
+console.log("add", add(2,3)); // expects 5
+
+/**
+
+Write a function that takes any number of arguments of type number and returns the sum of all the arguments
+ */
+
+function sum() {}
+
+console.log("sum", sum(2,3,4,5)); // expects 14
